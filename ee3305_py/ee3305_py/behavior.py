799c0c712b823827c4a78dde8ede3714a9143d5c
Mikidokido <e0486961@u.nus.edu>
2025-11-01 13:15:26 +0800

    hihihi

--- ee3305_py/ee3305_py/behavior.py
+++ ee3305_py/ee3305_py/behavior.py
@@ -6,7 +6,7 @@ from rclpy.qos import qos_profile_sensor_data, qos_profile_services_default
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Odometry, Path
 
-print("TEST TEST FOR CHENYI")
+print("TEST TEST FOR CHENGYI")
 
 class Behavior(Node):
 
